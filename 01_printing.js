1291f29e6f47ad1de91f0c740b0f84c42a537749
ahmedirfan1074 <ahmedirfan1074@gmail.com>
2025-08-14 21:24:08 +0530

    output

---
+++ 01_printing.js
@@ -0,0 +1,12 @@
+// give output without line break
+process.stdout.write("hello");
+process.stdout.write("world");
+
+// give output with line break
+console.log();
+
+console.log("hello");
+console.log("world");
+
+// give output in tabular form
+console.table({ city: "Delhi" });
