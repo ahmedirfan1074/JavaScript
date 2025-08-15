9797b17c6300a7ea1da510e658ef057fdc218df4
ahmedirfan1074 <ahmedirfan1074@gmail.com>
2025-08-15 15:30:49 +0530

    operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,51 @@
+// operations
+let a = 3 + 4;
+let b = 2 - 1;
+let c = 4 * 5;
+let d = 7 / 2;
+let e = 5 % 3;
+let f = 4 ** 3; // ** power operator
+
+// increment/decrement operator
+let g = 110;
+g++; // post-incremented
+++g; //pre-incremented
+
+let h = 52;
+h--; // post-decremented
+--h; // pre-decremented
+
+// comparision operator
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num1 != num2);
+console.log(num1 >= num2);
+console.log(num2 <= num3);
+
+// logical operator
+
+// && gives true when all statement are true
+// || gives true when atleast one statement is true
+// !  not operator
+
+// assignment operator
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+let isLoggedin = true;
+let ispaid = false;
+
+console.log(isLoggedin && ispaid);
+
+let isGoogleuser = true;
+let isMailuser = false;
+
+console.log(isGoogleuser || isMailuser);
