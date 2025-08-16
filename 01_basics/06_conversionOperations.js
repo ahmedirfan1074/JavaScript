2793873abbfa0959d63430a670b29bd9608cd826
ahmedirfan1074 <ahmedirfan1074@gmail.com>
2025-08-16 19:48:25 +0530

    commit

---
+++ 01_basics/06_conversionOperations.js
@@ -0,0 +1,12 @@
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + 1) // 2
+console.log("1" + "1") // 11
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
