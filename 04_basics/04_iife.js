785b641331c8fd045b76bc31fd62422d50f6a50f
ahmedirfan1074 <ahmedirfan1074@gmail.com>
2025-08-29 17:25:19 +0530

    iife

---
+++ 04_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+// primarily used for scope management and encapsulation
+// iife creates their own private scope which help avoiding global scope pollution
+
+// syntax  ()()  // in this the first parenthesis is for funtion and the second one is for exection
+ 
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+// in writng iife it is must to use semicolon after the line end bcz iife invokes the function but it does not tell where to stop. 
+
+// simple or un-named iife
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh');
